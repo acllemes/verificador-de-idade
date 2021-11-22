6126ebed02ba32f5c89d6b9a91247580b4368ef3
Andressa Lemes <89918957+acllemes@users.noreply.github.com>
2021-11-22 14:02:48 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    }
+    else 
+    {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+         var gênero = ''
+        if (fsex[0].checked) {
+            gênero = 'homem'
+        }
+        else if (fsex[1].checked) {
+            gênero = 'mulher'
+        }
+        res.innerHTML = `Detectamos que é ${gênero} com ${idade} anos de idade.`
+    }
+}
