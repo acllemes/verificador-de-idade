c5e535963b59a09bc318ca0a110c828524b51411
Andressa Lemes <89918957+acllemes@users.noreply.github.com>
2021-11-23 14:35:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,53 @@ function verificar() {
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
 
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
+    //aqui só será mostrado se o ano for menor que o atual
+    if (fano.value.length == 0 || Number(fano.value) > ano) { 
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     }
     else 
     {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-         var gênero = ''
+        var fsex = document.getElementsByName('radsex') // selecionando o sexo da pessoa
+        var idade = ano - Number(fano.value) //calculo da idade , o getFullYear e faz menos o ano indicado pelo usuario
+        var gênero = ''
+        var img = document.createElement('img') //pegando a imagem
+        img.setAttribute('id', 'foto') //mesmo que fosse fazer no html
         if (fsex[0].checked) {
             gênero = 'homem'
+
+            if (idade >=0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'img/homem_crianca.png')
+            } else if (idade < 21){
+                //jovem
+                img.setAttribute('src', 'img/homem_adolecente.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'img/homem_adulto.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'img/homem_idoso.png')
+            }
         }
         else if (fsex[1].checked) {
             gênero = 'mulher'
+
+            if (idade >=0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'img/mulher_crianca.png')
+            } else if (idade < 21){
+                //jovem
+                img.setAttribute('src', 'img/mulher_adolecente.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'img/mulher_adulta.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'img/mulher_idosa.png')
+            }
         }
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos que é ${gênero} com ${idade} anos de idade.`
+        res.appendChild(img)
     }
 }
